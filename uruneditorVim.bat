6308edb29d9121923f53613407f71fa0f5e69adb
George Loo <gloosg@yahoo.com.sg>
2018-03-30 22:45:09 +0800

    vim batch

--- uruneditorVim.bat
+++ uruneditorVim.bat
@@ -1,5 +1,5 @@
 
 echo off
 cls
-C:\Portableapps\PortableApps\gVimPortable\gVimPortable.exe -p *.go
+C:\Portableapps\PortableApps\gVimPortable\gVimPortable.exe -p *.go *.txt
 rem pause server.go client.go
